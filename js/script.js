efd88048cca484b997d31d02ffce8face7ac80d7
KlaudiaGibek <124783895+KlaudiaGibek@users.noreply.github.com>
2023-02-17 10:56:57 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar')
+	const navList = document.querySelector('.navbar-collapse')
+	const allNavItems = document.querySelectorAll('.nav-link')
+
+	function addShadow() {
+		if (window.scrollY >= 100) {
+			nav.classList.add('shadow-bg')
+		} else {
+			nav.classList.remove('shadow-bg')
+		}
+	}
+
+	console.log(allNavItems)
+
+	allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+	window.addEventListener('scroll', addShadow)
+})
